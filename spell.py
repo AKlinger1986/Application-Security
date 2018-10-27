b302a3769ed6880009c2fc42a535e091dfde2058
AKlinger1986 <adamjklinger@gmail.com>
2018-10-27 14:29:41 -0400

    Spell.py
    Add Password Quality Check

--- spell.py
+++ spell.py
@@ -9,6 +9,7 @@ from contextlib import suppress
 import hashlib
 import os
 import easygui
+import re
 
 app = Flask(__name__)
 
@@ -34,38 +35,45 @@ def home():
 @app.route("/login", methods=['POST'])
 def do_Login():
 	error = None
-	
+
 	#Obtain values from form
 	username = request.form['username']
-	#password = sha256_crypt.encrypt(request.form['password'])
 	password = request.form['password']
+				
+	# Validate that the Username  contains only Alphanumeric characters
+	if (username.isalnum() == False):
+		return render_template('login.html', error='Error! Username may only contain A-Z, a-z, 0-9.')
+	elif (( len(username) < 4) | (len(username) > 20) ):
+		return render_template('login.html', error='Error! Username must be between 4 to 20 characters in length')
 
+	# Validate the password chosen meets syntax rules
+	validPwd, errorMsg = password_Quality_Check(password)
+	if validPwd == False:
+		return render_template('login.html', error=errorMsg)
+		
 	if "Login" in request.form:
-	
-		#Check Credentials Against Database 			
-		if(password_Check(username,password)) == true:	
-			session['logged_in'] = True
-			return home()
-		else:
+		#Check Credentials Against Database 
+		if( (username_Check(username) != True)):
+			return render_template('login.html', error='Login Error -- Invalid Username or Password.')	
+		
+		if (password_Match_Check(username, password != True)):
 			return render_template('login.html', error='Login Error -- Invalid Username or Password.')
-		
+		#else:
+		session['logged_in'] = True
+		return home()		
 	elif "Register" in request.form:
-	
-		#Check to make sure the Username does not already exist is not already found
-		if (username_Check(username)) == false:
-		
-			if(len(password)) < 8: 
-				return render_template('login.html', error='Register Error -- Password value is too short. Please select a minimum length of 8.')
-			else:				
-				user = User(username,password)
-				s.add(user)
-				s.commit()
-				return render_template('login.html', error='Username '+username+' successfully registered.')
+		#Check to make sure the Username does not already exist
+		if (username_Check(username)) == False:				
+			user = User(username,password)
+			s.add(user)
+			s.commit()
+			return render_template('login.html', error='Username '+username+' successfully registered.')
 		else:
 			return render_template('login.html', error='Username '+username+' already registered. Please select a new value.')
 	else:
 		pass	
 	return home()
+
 
 @app.route("/spellcheck", methods=['POST'])
 def do_spellCheck():
@@ -134,18 +142,39 @@ def username_Check(POST_USERNAME):
 	query = s.query(User).filter(User.username.in_([POST_USERNAME]))
 	result = query.first()
 	if result:
-		return true
+		return True
 	else:
-		return false
+		return False
  
-def password_Check(POST_USERNAME,POST_PASSWORD):
+def password_Match_Check(POST_USERNAME,POST_PASSWORD):
 	query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
 	result = query.first()
 	if result:
-		return true
+		return True
 	else:
-		return false
+		return False
 
+def password_Quality_Check(POST_PASSWORD):
+	errorMsg = ("Password Rules not met. Length must be between 8-20 and contain at least"
+		        " one Uppercase [A-Z], one Lowercase [a-z], one Number [0-9], "  
+				" and one special character [@#$]. No other characters allowed.")
+				
+	if (len(POST_PASSWORD)<8):
+		return False, errorMsg
+	elif (len(POST_PASSWORD)>20):
+		return False, errorMsg
+	elif not re.search("[a-z]", POST_PASSWORD): 
+		return False, errorMsg
+	elif not re.search("[A-Z]", POST_PASSWORD): 
+		return False, errorMsg
+	elif not re.search("[0-9]", POST_PASSWORD): 
+		return False, errorMsg
+	elif not re.search("[_@#$]", POST_PASSWORD): 
+		return False, errorMsg
+	elif re.search("\s", POST_PASSWORD): 
+		return False, errorMsg	 
+	return True, ""
+		
 if __name__ == "__main__":
 	app.secret_key = os.urandom(12)
 	app.run(host='0.0.0.0', port=4000, debug=True)
